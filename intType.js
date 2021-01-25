d2d8eeb0e4f668144d79c12bb9b2e272da8ba672
ksdhk <47198182+ksdhk@users.noreply.github.com>
2021-01-26 00:15:41 +0900

    int_Type
    type test

---
+++ intType.js
@@ -0,0 +1,13 @@
+
+var integer = 10;
+var dobule = 10.12;
+var negative = -20;
+
+var binary = 0b01000001;
+var octal = 0o101;
+var hex = 0x41;
+
+
+console.log(binary);
+console.log(octal);
+console.log(hex);
