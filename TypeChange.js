b184a8051040db931b006e816a76b982dae71fab
ksdhk <ksdhk@naver.com>
2021-01-28 01:39:49 +0900

    type_test
    test

---
+++ TypeChange.js
@@ -0,0 +1,12 @@
+console.log(1+'2');
+console.log(1-'1');
+console.log(1*'10');
+console.log('1'>9);
+
+(true).toString();
+
+Number('0');
+
+parseInt('-1');
++'0';
+
---
+++ optionalChaining.js
@@ -0,0 +1,5 @@
+var elem = null;
+
+var value = elem?.value;
+
+console.log(value);
